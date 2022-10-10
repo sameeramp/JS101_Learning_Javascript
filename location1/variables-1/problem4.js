0f6dc372bacc94d29aeb3ed40a9d74aad6f99109
sameeramp <sameeramp2000@gmail.com>
2022-10-10 18:14:08 +0530

    commited successfully

---
+++ location1/variables-1/problem4.js
@@ -0,0 +1,24 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let name="sameera"
+let school="masai"
+let grade="A"
+let section="fullstack devolepment"
+var rollno=329
+var dsa=30
+var js=40
+var skill=30
+console.log("\n")
+console.log("******************************")
+console.log("\n")
+console.log("    𝑠𝑐ℎ𝑜𝑜𝑙  :  ", school)
+console.log("    𝘨𝘳𝘢𝘥𝘦   :  ",grade)
+console.log("    𝑠𝑒𝑐𝑡𝑖𝑜𝑛 :  ", section)
+console.log("    𝑟𝑜𝑙𝑙𝑛𝑜  :  ",rollno)
+console.log("    𝑑𝑠𝑎     :  ",dsa)
+console.log("    𝑗𝑠      :  ",js)
+console.log("    𝑠𝑘𝑖𝑙𝑙   :  ",skill)
+console.log("\n")
+console.log("******************************")
+console.log("\n")
